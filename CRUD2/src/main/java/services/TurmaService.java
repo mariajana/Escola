cc8ce6a40b9968b8758959297eec564ecf7e9c0a
mariajana <43685759+mariajana@users.noreply.github.com>
2018-10-26 23:33:17 -0300

    Update TurmaService.java

--- CRUD2/src/main/java/services/TurmaService.java
+++ CRUD2/src/main/java/services/TurmaService.java
@@ -1,61 +1,55 @@
 package services;
 
+import java.hugo.daw.crud.Turma;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import dao.TurmaDAO;
+import util.TransacionalCdi;
+
 public class TurmaService {
 	
 	@ApplicationScoped
-	public class TurmaService implements Serializable, Service<Usuario> {
+	public class TurmaService implements Serializable, Service<Turma> {
 
-	/**
-	* 
-	*/
 	private static final long serialVersionUID = -7803325791425670859L;
 		
 	@Inject
 	private TurmaDAO turmaDAO;
 		
-	/* (non-Javadoc)
-	* @see br.edu.ifpb.esperanca.daw2.services.Service#save(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario)
-	*/
 	@Override
 	@TransacionalCdi
 	public void save(Turma turma) {
 		turmaDAO.save(turma);
 	}
 
-	/* (non-Javadoc)
-	* @see br.edu.ifpb.esperanca.daw2.services.Service#update(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario, boolean)
-	 */
 	@Override
 	@TransacionalCdi
 	public void update(Turma turma)  {
-			turmaDAO.update(turma);
+		turmaDAO.update(turma);
 	}
 
-	/* (non-Javadoc)
-	* @see br.edu.ifpb.esperanca.daw2.services.Service#delete(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario)
-	*/
 	@Override
 	@TransacionalCdi
-	public void remove(Usuario user) {
-		userDAO.remove(user);
+	public void remove(Turma turma) {
+		turmaDAO.remove(turma);
 	}
 
-	/* (non-Javadoc)
-	* @see br.edu.ifpb.esperanca.daw2.services.Service#getByID(long)
-	*/
 	@Override
-	public Usuario getByID(long userId)  {
-			return userDAO.getByID(userId);
+	public Turma getByID(long turmaId)  {
+		return turmaDAO.getByID(turmaId);
 	}
 
-	/* (non-Javadoc)
-	 * @see br.edu.ifpb.esperanca.daw2.services.Service#getAll()
-	 */
 	@Override
-	public List<Usuario> getAll() {
-			return userDAO.getAll();
+	public List<Turma> getAll() {
+		return turmaDAO.getAll();
 	}
 			
-}
+	}
 
 }
