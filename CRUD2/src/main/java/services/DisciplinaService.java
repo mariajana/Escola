b0c83ef5032f26757e922e9e7ea4dfd378853a52
mariajana <43685759+mariajana@users.noreply.github.com>
2018-10-26 23:28:46 -0300

    Update DisciplinaService.java

--- CRUD2/src/main/java/services/DisciplinaService.java
+++ CRUD2/src/main/java/services/DisciplinaService.java
@@ -1,7 +1,51 @@
 package services;
 
-public class DisciplinaService {
-  
-  
+import java.hugo.daw.crud.Disciplina;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import dao.DisciplinaDAO;
+import util.TransacionalCdi;
+
+@ApplicationScoped
+public class DisciplinaService implements Serializable, Service<Disciplina> {
+
+	private static final long serialVersionUID = -7803325791425670859L;
+
+	@Inject
+	private DisciplinaDAO disciplinaDAO;
+
+	@Override
+	@TransacionalCdi
+	public void save(Disciplina disciplina) {
+		disciplina.setPassword(hash(disciplina.getPassword()));
+		disciplinaDAO.save(disciplina);
+	}
+
+	@Override
+	@TransacionalCdi
+	public void update(Disciplina disciplina) {
+		disciplinaDAO.update(disciplina);
+	}
+
+	@Override
+	@TransacionalCdi
+	public void remove(Disciplina disciplina) {
+		disciplinaDAO.remove(disciplina);
+	}
+
+	@Override
+	public Disciplina getByID(long disciplinaId) {
+		return disciplinaDAO.getByID(disciplinaId);
+	}
+
+	@Override
+	public List<Disciplina> getAll() {
+		return disciplinaDAO.getAll();
+	}
 }
